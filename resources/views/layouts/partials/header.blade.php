04c2ca9810d7dd2dd21e7e1f25aee7ac4ccef085
Marcus Reis <mvsantosreis@gmail.com>
2025-01-04 00:21:56 +0000

    feat profile menu

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -45,16 +45,12 @@
     </div>
     <div id="header-right" class="flex items-center md:space-x-6">
         @guest
-            <div class="flex space-x-5">
-                <a class="flex items-center space-x-2 text-sm text-gray-500 hover:text-yellow-500"
-                    href="http://127.0.0.1:8000/login">
-                    Login
-                </a>
-                <a class="flex items-center space-x-2 text-sm text-gray-500 hover:text-yellow-500"
-                    href="http://127.0.0.1:8000/register">
-                    Register
-                </a>
-            </div>
+            @include('layouts.partials.header-right-guest')
         @endguest
+
+        @auth()
+        @include('layouts.partials.header-right-auth')
+        @endauth
+
     </div>
 </header>
