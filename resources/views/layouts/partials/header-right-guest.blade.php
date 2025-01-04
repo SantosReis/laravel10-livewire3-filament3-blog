43713bff04ace51972d198e7119c54ce53e049e8
Marcus Reis <mvsantosreis@gmail.com>
2025-01-04 19:00:57 +0000

    Logo Components

--- resources/views/layouts/partials/header-right-guest.blade.php
+++ resources/views/layouts/partials/header-right-guest.blade.php
@@ -1,10 +1,10 @@
 <div class="flex space-x-5">
     <a class="flex items-center space-x-2 text-sm text-gray-500 hover:text-yellow-500"
-        href="http://127.0.0.1:8000/login">
+        href="{{ route('login') }}">
         Login
     </a>
     <a class="flex items-center space-x-2 text-sm text-gray-500 hover:text-yellow-500"
-        href="http://127.0.0.1:8000/register">
+        href="{{ route('register') }}">
         Register
     </a>
 </div>
