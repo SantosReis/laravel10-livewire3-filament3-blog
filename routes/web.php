d4e98fd6335c8ca0dba43d60245f70ad3661740b
Marcus Reis <mvsantosreis@gmail.com>
2025-01-05 10:52:14 +0000

    Routing Post main page with wire:navigate

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,9 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', HomeController::class)->name('home');
+
+Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
+
 
 Route::middleware([
     'auth:sanctum',
