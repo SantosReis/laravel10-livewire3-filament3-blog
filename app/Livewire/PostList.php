a4ae5a763e83375e5fb2b4dcda8d5a6729407117
Marcus Reis <mvsantosreis@gmail.com>
2025-01-05 14:41:06 +0000

    PostList: Order by Latest or Olderst

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -4,16 +4,25 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
-use Livewire\Attributes\Computed;
+use Livewire\Attributes\Url;
 use Livewire\WithPagination;
+use Livewire\Attributes\Computed;
 
 class PostList extends Component
 {
     use WithPagination;
 
+    #[Url()]
+    public $sort = 'desc';
+
+    public function setSort($sort)
+    {
+        $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
+    }
+
     #[Computed()]
     public function posts(){
-        return Post::published()->orderBy('published_at', 'desc')->paginate(3);
+        return Post::published()->orderBy('published_at', $this->sort)->paginate(3);
         // return Post::published()->orderBy('published_at', 'desc')->simplePaginate(3);
     }
 
--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,8 +1,8 @@
 <div class="px-3 py-6 lg:px-7">
     <div class="flex items-center justify-between border-b border-gray-100">
         <div class="flex items-center space-x-4 font-light ">
-            <button class="py-4 text-gray-500">Latest</button>
-            <button class="py-4 text-gray-900 border-b border-gray-700">Oldest</button>
+            <button class="{{ $sort === 'desc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }} py-4" wire:click="setSort('desc')">Latest</button>
+            <button class="{{ $sort === 'asc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }} py-4" wire:click="setSort('asc')">Oldest</button>
         </div>
     </div>
     <div class="py-4">
