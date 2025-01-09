0b0344a78bafec87c1362e41f422230f2f0be588
Marcus Reis <mvsantosreis@gmail.com>
2025-01-09 00:16:25 +0000

    Category items on Sidebar

---
+++ resources/views/posts/partials/categories-box.blade.php
@@ -0,0 +1,9 @@
+<div id="recommended-topics-box">
+    <h3 class="mb-3 text-lg font-semibold text-gray-900">Recommended Topics</h3>
+    <div class="flex flex-wrap justify-start gap-2 topics">
+        @foreach ($categories as $category)
+            {{-- <x-badge textColor="red" bgColor="red">{{ $category->title }}</x-badge> --}}
+            <x-badge :textColor="$category->text_color" :bgColor="$category->bg_color">{{ $category->title }}</x-badge>
+        @endforeach
+    </div>
+</div>
