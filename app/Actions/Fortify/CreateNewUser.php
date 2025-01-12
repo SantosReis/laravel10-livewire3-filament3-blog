30206183635b17e72750f865d257e9329b409041
Marcus Reis <mvsantosreis@gmail.com>
2025-01-12 12:25:22 +0000

    Menu Admin Access

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -33,6 +33,7 @@ class CreateNewUser implements CreatesNewUsers
                 'name' => $input['name'],
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
+                'role' => User::ROLE_DEFAULT,
             ]), function (User $user) {
                 $this->createTeam($user);
             });
