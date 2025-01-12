fb92a83ba5c4109048f6c412303f3a00b3dac13e
Marcus Reis <mvsantosreis@gmail.com>
2025-01-12 12:06:25 +0000

    Policies

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,7 +36,8 @@ class User extends Authenticatable implements FilamentUser
     public function canAccessPanel(Panel $panel): bool
     {
         // return $this->role === self::ROLE_ADMIN || $this->role === self::ROLE_EDITOR;
-        return $this->isAdmin() || $this->isEditor();
+        // return $this->isAdmin() || $this->isEditor();
+        return $this->can('view-admin', User::class);
     }
 
     public function isAdmin(){
