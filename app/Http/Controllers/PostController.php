e0d54eaac3600397e2fd648bd720038aa11b9a8c
Marcus Reis <mvsantosreis@gmail.com>
2025-01-19 05:12:28 +0000

    Caching database requests

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
     public function index()
     {
+
+        $categories = Cache::remember('categories', now()->addDays(3), function () {
+            return Category::whereHas('posts', function ($query) {
+                $query->published();
+            })->take(10)->get();
+        });
+
         return view('posts.index',
             [
             // 'posts' => Post::take(5)->get(),
-                'categories' => Category::whereHas('posts', function ($query) {
-                    $query->published();
-                })->take(10)->get()
+                'categories' => $categories
             ]
         );
     }
