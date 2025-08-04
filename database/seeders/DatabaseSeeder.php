905ee4606e818dadb1687db9425d11bc2f363366
Marcus Reis <mvsantosreis@gmail.com>
2025-08-04 22:09:36 +0100

    Update seeders and include CommentFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +13,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Post::factory(100)->create();
-        \App\Models\Category::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create admin
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@mail.com',
+        ]);
+
+        // Create Categories
+        $categories = \App\Models\Category::factory(5)->create();
+
+
+        $faker = Faker::create();
+
+        $tagNames = collect(range(1, 5))->map(fn() => $faker->unique()->word());
+
+        $tagNames = ['Laravel', 'Vue', 'Livewire', 'Backend', 'Frontend'];
+        $tags = collect($tagNames)->map(fn($name) => \App\Models\Tag::findOrCreate($name));
+
+        $users = \App\Models\User::factory(10)->create();
+
+        // Create posts
+        \App\Models\Post::factory(100)->create()->each(function ($post) use ($categories, $tags, $users) {
+            // Attach 1-3 random categories
+            $post->categories()->attach(
+                $categories->random(rand(1, 3))->pluck('id')->toArray()
+            );
+
+            // Assign some tags
+            $post->attachTags($tags->random(rand(1, 2))->pluck('name')->toArray());
+
+            // Optionally assign to admin
+            $post->author()->associate($users->random())->save();
+
+            // Add comments by random users
+            \App\Models\Comment::factory(rand(2, 6))->create([
+                'post_id' => $post->id,
+                'user_id' => $users->random()->id,
+            ]);
+        });
+
     }
 }
